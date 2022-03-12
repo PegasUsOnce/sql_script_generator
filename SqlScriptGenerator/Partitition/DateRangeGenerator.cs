9579ef37d066ad92d332c4a1afe1b412d1923271
PegasUsOnce <rosliakovaov@gmail.com>
2022-03-12 18:26:33 +1000

    написала новый генератор дат и проверила тестом производительность

---
+++ SqlScriptGenerator/Partitition/DateRangeGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SqlScriptGenerator.Partitition
+{
+    public static class DateRangeGenerator
+    {
+        public static ICollection<DateTime> GenerateDates(int quantity, DateTime startDate)
+        {
+            return Enumerable.Range(1, quantity - 1)
+                .Select(x => new DateTime(
+                    startDate.Year + (int)((startDate.Month + x) / 13),
+                    (startDate.Month + x) % 13 + ((startDate.Month + x) / 13),
+                    1))
+                .Prepend(startDate)
+                .ToArray();
+        }
+
+        public static ICollection<DateTime> GenerateDatesByMonth(int quantity, DateTime startDate)
+        {
+            var dates = new List<DateTime>(quantity);
+            dates.Add(startDate);
+
+            var start = new DateTime(startDate.Year, startDate.Month, 1);
+            for (int i = 1; i < quantity; i++)
+            {
+                dates.Add(start.AddMonths(i));
+            }
+
+            return dates;
+        }
+
+
+        public static ICollection<DateTime> GenerateDatesByMonthArray(int quantity, DateTime startDate)
+        {
+            var dates = new DateTime[quantity];
+            dates[0] = startDate;
+
+            var start = new DateTime(startDate.Year, startDate.Month, 1);
+            for (int i = 1; i < quantity; i++)
+            {
+                dates[i] = start.AddMonths(i);
+            }
+
+            return dates;
+        }
+    }
+}
---
+++ TestProject/DateRandeGeneratorTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using SqlScriptGenerator.Partitition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestProject
+{
+    [TestFixture, Parallelizable(ParallelScope.All)]
+    public class DateRandeGeneratorTests
+    {
+        [Test]
+        public void Generate_March(
+            [Values(1, 15, 31)] int day, 
+            //[Values(1, 5, 12, 54, 100, 1000)] int quantity)
+            [Values(1, 100, 1000, 10000)] int quantity)
+        {
+            var start = new DateTime(2022, 3, 1);
+            var dates = new List<DateTime>(quantity);
+            dates.Add(new DateTime(start.Year, start.Month, day));
+
+            for (int i = 1; i < quantity; i++)
+            {
+                dates.Add(start.AddMonths(i));
+            }
+
+            var result = DateRangeGenerator.GenerateDatesByMonth(quantity, new DateTime(start.Year, start.Month, day)).ToList();
+
+            Assert.AreEqual(dates.Count, result.Count);
+            for (var i = 0; i < dates.Count; i++)
+            {
+                Assert.AreEqual(dates[i], result[i]);
+                if (i > 0)
+                    Assert.AreNotEqual(result[i - 1], result[i]);
+            }
+        }
+
+        [Test]
+        public void Generate_March1(
+            [Values(1, 15, 31)] int day,
+            //[Values(1, 5, 12, 54, 100, 1000)] int quantity)
+            [Values(1, 100, 1000, 10000)] int quantity)
+        {
+            var start = new DateTime(2022, 3, 1);
+            var dates = new List<DateTime>(quantity);
+            dates.Add(new DateTime(start.Year, start.Month, day));
+
+            for (int i = 1; i < quantity; i++)
+            {
+                dates.Add(start.AddMonths(i));
+            }
+
+            var result = DateRangeGenerator.GenerateDatesByMonthArray(quantity, new DateTime(start.Year, start.Month, day)).ToList();
+
+            Assert.AreEqual(dates.Count, result.Count);
+            for (var i = 0; i < dates.Count; i++)
+            {
+                Assert.AreEqual(dates[i], result[i]);
+                if (i > 0)
+                    Assert.AreNotEqual(result[i - 1], result[i]);
+            }
+        }
+    }
+}
